6fb52cded97f6a490c263f9cec8076336c112c05
Jason Bryer <jason@bryer.org>
2021-02-11 12:14:04 -0500

    Updated shiny app and likelihood plots.

--- R/plot_likelihood.R
+++ R/plot_likelihood.R
@@ -20,20 +20,29 @@ plot_likelihood <- function(x, y, height = 0.4, k = 4, pt = 1,
 	x0 <- x[pt]
 	y0 <- slope * x0 + intercept
 	path <- data.frame(x = yvals + x0, y = xvals + y0)
-	segment <- data.frame(x = x0, y = y0 - k*sigma, xend = x0, yend = y0 + k*sigma)
+	segment <- data.frame(x = x0,
+						  y = y0 - k*sigma,
+						  xend = x0,
+						  yend = y0 + k*sigma)
 	segment2 <- data.frame(x = x0,
 						   y = y[pt],
 						   xend = dnorm(y0 - abs(y[pt]), 0, sigma) / dnorm(0, 0, sigma) * heightFactor + x0,
 						   yend = y[pt])
+	segment3 <- data.frame(x = x0,
+						   y = y0,
+						   xend = dnorm(0, 0, sigma) / dnorm(0, 0, sigma) * heightFactor + x0,
+						   yend = y0)
 
 
 	p <- ggplot(data.frame(x = x, y = y), aes(x = x, y = y)) +
 		geom_point(size = 0.25) +
 		geom_abline(intercept = intercept, slope = slope, alpha = 1, color = '#ff7f00') +
+		geom_segment(data = segment3, aes(x = x, y = y, xend = xend, yend = yend), alpha = 0.3) +
 		geom_segment(data = segment, aes(x = x, y = y, xend = xend, yend = yend), alpha = 0.5) +
-		geom_segment(data = segment2, aes(x = x, y = y, xend = xend, yend = yend), color = 'red') +
+		geom_segment(data = segment2, aes(x = x, y = y, xend = xend, yend = yend), color = '#e31a1c') +
 		geom_vline(xintercept = x0) +
-		geom_point(data = data.frame(x = x[pt], y = y[pt]), color = '#e31a1c', size = 1) +
+		geom_point(data = data.frame(x = x[pt], y = y[pt]), color = '#e31a1c', size = 1.5) +
+		geom_point(data = data.frame(x = segment2$xend, y = y[pt]), color = '#e31a1c', size = 1.5) +
 		geom_path(data = path, aes(x = x, y = y), color = "#33a02c")
 	return(p)
 }
